410da7c042cf6359c4a4c437ec0a35cfb5607859
Ben Story <story.benjamin@gmail.com>
2014-07-22 13:11:09 -0500

    added dataloader biomart var

---
+++ dataLoader.R
@@ -0,0 +1,31 @@
+##################################################
+## load the data
+##################################################
+source("helpers.R")
+
+##################################################
+## Read the different ROI files (GRanges objects)
+##################################################
+ROIfiles <- list.files('data','_ROI.rds$',full=TRUE)
+ROIs <- lapply(ROIfiles,readRDS)
+names(ROIs) <- sub('_ROI.rds$',"",basename(ROIfiles))
+
+##################################################
+## Read in the coverage files (RleLists of coverages)
+##################################################
+dir <- 'data/cov'
+files <- list.files(dir,"_cov\\.rds$",full=TRUE)
+covs <- structure(files,names=sub("_cov.+","",basename(files)))
+
+
+cov.data <- mclapply(covs,readRDS,mc.cores=25,mc.preschedule=FALSE)
+names(cov.data) <- sub("_cov\\.rds","",basename(covs))
+
+
+##################################################
+## read gene information pulled from biomart (data.frame)
+##################################################
+ids <- readRDS("data/martIDs.rds")
+
+##add mart
+linkOut <- 'http://flybase.org/reports/'
